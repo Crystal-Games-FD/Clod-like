e638b3264722ecf5cd7b39bbcb128470445d3a3f
Fipaan <git@github.com:Crystal-Games-FD>
2026-05-10 20:56:33 +0500

    Added stuff

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -1,17 +1,35 @@
+using System;
 using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    [Header("Prefabs")]
-    [SerializeField] private Transform backgroundPrefab;
-
+    [Serializable]
+    public class Side {
+        public enum Kind {
+            Wall,
+            Door,
+            Any,
+        };
+        [Header("Properties")]
+        [SerializeField] public Kind kind;
+        public Side() {
+            kind = Kind.Any;
+        }
+    }
+    [Header("Properties")]
+    [SerializeField] private Zone.Kind kind = Zone.Kind.None;
+    [SerializeField] private Side left  = new();
+    [SerializeField] private Side right = new();
+    [SerializeField] private Side up    = new();
+    [SerializeField] private Side down  = new();
+    private Side[] sides; 
     void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        sides = new Side[]{
+            left,
+            right,
+            up,
+            down,
+        };
     }
 }
